2bfeb78597fbda1bd14157f93d9677c201d94891
petribrn <brunoopetri@gmail.com>
2022-07-31 20:20:23 -0300

    completed mvp of tic tac toe game

---
+++ README.md
@@ -0,0 +1,23 @@
+# tic-tac-toe
+
+O projeto consiste em uma aplicação web que permite que o usuário jogue um "jogo da velha". Nessa aplicação, os dois jogadores inserem seus nomes e começam a jogatina, um sendo representado pelo símbolo "X" e outro pelo "O", e suas pontuações são mostradas em um placar. Além disso, o programa alterna qual dos dois jogadores iniciará cada partida. Quando um jogador vence, a sequência que concretizou sua vitória é realçada com linhas verdes em volta. Também é possível que os usuários alterem seus nomes no jogo, sem que suas pontuações sejam perdidas. Além disso, é possível resetar a partida em qualquer momento assim como a pontuação dos jogadores.
+
+O projeto conta com apenas uma página HTML que importa um arquivo CSS com estilizações extras (style.css) e um arquivo JS com a lógica do jogo (/assets/js/index.js). Foi usado também para ajudar a estilização o framework Bootstrap 5.
+
+## Alunos
+
+- Bruno Petri (21106217)
+- Carolina Pacheco da Silva (21104291)
+- Guilherme Cavalieri
+- Vinícius de Araújo Portela (21105982)
+
+## Divisão de tarefas
+
+- Bruno Petri (21106217)
+    - Estrutura HTML com uso de classes Bootstrap 5.
+- Carolina Pacheco da Silva (21104291)
+    - Design, layout, cores, fontes e estilização.
+- Guilherme Cavalieri
+    - CSS.
+- Vinícius de Araújo Portela (21105982)
+    - Javascript, classe TicTacToe.
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,28 +4,6 @@ const O_MARK = 'O'
 const FIRST_PLAYER = 'FIRST_PLAYER'
 const SECOND_PLAYER = 'SECOND_PLAYER'
 
-class TicTacToeNodes {
-  static getCell(position) {
-    return document.getElementById(`cell-${position.x}-${position.y}`)
-  }
-
-  static getStartGameButton() {
-    return document.getElementById('init-game-button')
-  }
-
-  static getStartUpdatePlayersButton() {
-    return document.getElementById('start-update-players')
-  }
-
-  static getUpdatePlayersButton() {
-    return document.getElementById('update-players')
-  }
-
-  static getPlayersForm() {
-    return document.getElementById('players-form')
-  }
-}
-
 class TicTacWinPossibilities {
   static generate() {
     const winPossibilities = [];
@@ -38,10 +16,10 @@ class TicTacWinPossibilities {
   }
 
   static getWinningCondition(board, marker, winPossibilities) {
-    for (let cellsToWin in winPossibilities) {
+    for (let cellsToWin of winPossibilities) {
       let winCellMatchedCount = 0;
 
-      for (let cell in cellsToWin) {
+      for (let cell of cellsToWin) {
         if (board[cell.x][cell.y] === marker) {
           winCellMatchedCount += 1;
         } else {
@@ -100,6 +78,86 @@ class TicTacWinPossibilities {
   }
 }
 
+class TicTacToeNodes {
+  static getCell(position) {
+    return document.getElementById(`cell-${position.x}-${position.y}`)
+  }
+
+  static getStartGameButton() {
+    return document.getElementById('init-game-button')
+  }
+
+  static getChangePlayerNamesButton() {
+    return document.getElementById('change-players-names')
+  }
+
+  static getUpdatePlayersButton() {
+    return document.getElementById('update-players')
+  }
+
+  static getPlayersForm() {
+    return document.getElementById('players-form')
+  }
+
+  static getPlayersInputs() {
+    const firstPlayerInput = document.getElementById('player-1-name')
+    const secondPlayerInput = document.getElementById('player-2-name')
+
+    return [firstPlayerInput, secondPlayerInput]
+  }
+
+  static getScoreboard() {
+    const scoreboard = document.getElementById('scoreboard')
+
+    const firstPlayerName = document.getElementById('first-player-name')
+    const firstPlayerScore = document.getElementById('first-player-score')
+
+    const secondPlayerName = document.getElementById('second-player-name')
+    const secondPlayerScore = document.getElementById('second-player-score')
+
+    return {
+      node: scoreboard,
+      players: {
+        first: {
+          name: firstPlayerName,
+          score: firstPlayerScore
+        },
+        second: {
+          name: secondPlayerName,
+          score: secondPlayerScore
+        }
+      },
+    }
+  }
+
+  static getNodesWithPosition() {
+    const nodesWithPosition = [];
+
+    for (let columnIndex = 0; columnIndex < 3; columnIndex += 1) {
+      for (let rowIndex = 0; rowIndex < 3; rowIndex += 1) {
+        nodesWithPosition.push({
+          node: this.getCell({ x: columnIndex, y: rowIndex }),
+          position: { x: columnIndex, y: rowIndex }
+        });
+      }
+    }
+
+    return nodesWithPosition;
+  }
+
+  static getResetMatchButton() {
+    return document.getElementById('reset-match')
+  }
+
+  static getResetScoreboardButton() {
+    return document.getElementById('reset-scoreboard')
+  }
+
+  static getAnnouncer() {
+    return document.getElementById('announcer-section');
+  }
+}
+
 class TicTacToeRenderer {
   renderCell(position, marker) {
     const cellDiv = TicTacToeNodes.getCell(position);
@@ -124,22 +182,44 @@ class TicTacToeRenderer {
     }
   }
 
-  hidePlayerRegister() {
-    console.log('hide')
+  hidePlayersForm() {
     const playersForm = TicTacToeNodes.getPlayersForm();
-    playersForm.style.display = 'none !important';
+    playersForm.style.setProperty('display', 'none', 'important');
   }
 
-  showPlayerRegister() {
-    // TODO
+  showPlayersForm() {
+    const playersForm = TicTacToeNodes.getPlayersForm();
+    playersForm.style.setProperty('display', 'block', 'important')
   }
 
-  showWinScreen(winnerName) {
-    // TODO
+  showChangePlayerNamesButton() {
+    const changePlayerNamesButton = TicTacToeNodes.getChangePlayerNamesButton();
+    changePlayerNamesButton.style.setProperty('display', 'flex', 'important')
   }
 
-  showTieScreen() {
-    // TODO
+  hideChangePlayerNamesButton() {
+    const changePlayerNamesButton = TicTacToeNodes.getChangePlayerNamesButton();
+    changePlayerNamesButton.style.setProperty('display', 'none', 'important')
+  }
+
+  showUpdatePlayersButton() {
+    const updatePlayersButton = TicTacToeNodes.getUpdatePlayersButton();
+    updatePlayersButton.style.setProperty('display', 'flex', 'important')
+  }
+
+  hideUpdatePlayersButton() {
+    const updatePlayersButton = TicTacToeNodes.getUpdatePlayersButton();
+    updatePlayersButton.style.setProperty('display', 'none', 'important')
+  }
+
+  renderAnnouncer(message) {
+    const announcer = TicTacToeNodes.getAnnouncer();
+    announcer.textContent = message;
+  }
+
+  showAnnouncer() {
+    const announcer = TicTacToeNodes.getAnnouncer();
+    announcer.style.setProperty('display', 'flex', 'important');
   }
 
   disableCells() {
@@ -149,8 +229,6 @@ class TicTacToeRenderer {
       }
     }
   }
-
-  updateScore(players) {}
   
   resetHighlight(position) {
     const cellDiv = TicTacToeNodes.getCell(position)
@@ -159,7 +237,39 @@ class TicTacToeRenderer {
     cellDiv.classList.remove('text-success');
   }
 
-  renderScore(player) {}
+  showScoreboard() {
+    const scoreboard = TicTacToeNodes.getScoreboard().node;
+    scoreboard.style.setProperty('display', 'flex', 'important');
+  }
+
+  renderScoreboard(playersNameAndScore) {
+    const scoreboard = TicTacToeNodes.getScoreboard();
+
+    scoreboard.players.first.name.textContent = playersNameAndScore.firstPlayer.name;
+    scoreboard.players.first.score.textContent = playersNameAndScore.firstPlayer.score;
+
+    scoreboard.players.second.name.textContent = playersNameAndScore.secondPlayer.name;
+    scoreboard.players.second.score.textContent = playersNameAndScore.secondPlayer.score;
+  }
+
+  getPlayerNames() {
+    const playersInputs = TicTacToeNodes.getPlayersInputs();
+    
+    return [
+      playersInputs[0].value, 
+      playersInputs[1].value
+    ]
+  }
+
+  showResetMatchButton() {
+    const resetMatchButton = TicTacToeNodes.getResetMatchButton();
+    resetMatchButton.style.setProperty('display', 'flex', 'important');
+  }
+
+  hideStartGameButton() {
+    const startGameButton = TicTacToeNodes.getStartGameButton();
+    startGameButton.style.setProperty('display', 'none', 'important');
+  }
 }
 
 class TicTacToe {
@@ -167,7 +277,10 @@ class TicTacToe {
     this.board = null;
     this.players = { [FIRST_PLAYER]: null, [SECOND_PLAYER]: null };
     this.renderer = new TicTacToeRenderer();
-    this.firstToPlay = SECOND_PLAYER;
+    this.firstToPlayId = SECOND_PLAYER;
+    this.currentPlayerId = this.firstToPlayId;
+    this.gameStopped = true;
+    this.gameFinishedState = null;
   }
 
   initialize() {
@@ -177,11 +290,39 @@ class TicTacToe {
   }
 
   startGame() {
-    this.board = this._initializeBoard();
+    this.gameStopped = false;
+    this.gameFinishedState = null;
+
+    this._initializeBoard();
     this._switchFirstPlayer();
-    
+    this.currentPlayerId = this.firstToPlayId;
+
+    const firstToPlay = this.players[this.firstToPlayId];
+    this.renderer.renderAnnouncer(`Vez de ${firstToPlay.name}`);
+    this.renderer.hideStartGameButton();
+    this.renderer.showAnnouncer();
     this.renderer.renderBoard();
-    this.renderer.hidePlayerRegister();
+    this.renderer.hidePlayersForm();
+    this.renderer.renderScoreboard(this._buildScoreboardData());
+    this.renderer.showScoreboard();
+    this.renderer.showChangePlayerNamesButton();
+    this.renderer.showResetMatchButton();
+  }
+
+  updatePlayerNamesToInputs() {
+    const playersNames = this.renderer.getPlayerNames();
+    this.updatePlayerNames(playersNames[0], playersNames[1]);
+  }
+
+  updatePlayerNames(firstPlayerName, secondPlayerName) {
+    this.players[FIRST_PLAYER].name = firstPlayerName;
+    this.players[SECOND_PLAYER].name = secondPlayerName;
+  }
+
+  resetScoreboard() {
+    this.players[FIRST_PLAYER].score = 0;
+    this.players[SECOND_PLAYER].score = 0;
+    this.renderer.renderScoreboard(this._buildScoreboardData());
   }
 
   setPlayers(firstPlayerName, secondPlayerName) {
@@ -196,18 +337,23 @@ class TicTacToe {
     this._setupPlayerMarkers();
   }
 
-  updatePlayerNames(firstPlayerName, secondPlayerName) {
-    this.players[FIRST_PLAYER].name = firstPlayerName;
-    this.players[SECOND_PLAYER].name = secondPlayerName;
-  }
+  mark(position) {
+    if (this.gameStopped) return;
+    if (this.board[position.x][position.y]) return;
 
-  mark(player, position) {
+    const playerId = this.currentPlayerId;
+    const player = this.players[playerId];
+    this._switchPlayerRound();
+
+    const newPlayer = this.players[this.currentPlayerId];
+
     this.board[position.x][position.y] = player.marker;
-    this._renderCell(position);
+    this.renderer.renderCell(position, player.marker);
+    this.renderer.renderAnnouncer(`Vez de ${newPlayer.name}`);
 
-    const hasWon = TicTacWinPossibilities.getWinningCondition(this.board, player.marker, this.winPossibilities);
-    if (hasWon) {
-      this.onWon(player);
+    const winningCondition = TicTacWinPossibilities.getWinningCondition(this.board, player.marker, this.winPossibilities);
+    if (winningCondition) {
+      this.onWon(playerId, winningCondition);
       return;
     }
 
@@ -218,27 +364,84 @@ class TicTacToe {
     }
   }
 
-  onWon(winnerPlayerPosition) {
-    const winnerPlayer = this.players[winnerPlayerPosition];
+  onWon(winnerPlayerId, winningCondition) {
+    this.gameStopped = true;
+    this.gameFinishedState = {
+      state: 'won',
+      player: this.players[winnerPlayerId],
+    }
+
+    const winnerPlayer = this.players[winnerPlayerId];
     winnerPlayer.score += 1;
 
-    this.renderer.showWinScreen(winnerPlayer.name);
+    this.renderer.renderScoreboard(this._buildScoreboardData());
+    this.renderer.renderAnnouncer(`${winnerPlayer.name} venceu!`);
+    this.renderer.highlightWinningCells(winningCondition);
   }
 
   onTie() {
-    this.renderer.showTieScreen();
+    this.gameStopped = true;
+    this.gameFinishedState = {
+      state: 'tie'
+    }
+
+    this.renderer.renderAnnouncer(`Empate!`);
+  }
+
+  onClickChangePlayerNamesButton() {
+    this.renderer.hideChangePlayerNamesButton();
+    this.renderer.showPlayersForm();
+    this.renderer.showUpdatePlayersButton();
+  }
+
+  onClickUpdatePlayersButton() {
+    this.updatePlayerNamesToInputs();
+    this.renderer.renderScoreboard(this._buildScoreboardData());
+
+    const message = this.gameFinishedState ? 
+      this.gameFinishedState.state === 'won' ? 
+        `${this.gameFinishedState.player.name} venceu!` : 
+        `Empate!`
+      : `Vez de ${this.players[this.currentPlayerId].name}`;
+
+    this.renderer.renderAnnouncer(message);
+
+    this.renderer.showChangePlayerNamesButton();
+    this.renderer.hidePlayersForm();
+    this.renderer.hideUpdatePlayersButton();
+  }
+
+  _buildScoreboardData() {
+    return {
+      firstPlayer: {
+        name: this.players[FIRST_PLAYER].name,
+        score: this.players[FIRST_PLAYER].score
+      },
+      secondPlayer: {
+        name: this.players[SECOND_PLAYER].name,
+        score: this.players[SECOND_PLAYER].score
+      },
+    }
   }
 
   _switchFirstPlayer() {
-    if (this.firstToPlay === FIRST_PLAYER) {
-      this.firstToPlay = SECOND_PLAYER
+    if (this.firstToPlayId === FIRST_PLAYER) {
+      this.firstToPlayId = SECOND_PLAYER
     } else {
-      this.firstToPlay = FIRST_PLAYER
+      this.firstToPlayId = FIRST_PLAYER
+    }
+  }
+
+  _switchPlayerRound() {
+    if (this.currentPlayerId === FIRST_PLAYER) {
+      this.currentPlayerId = SECOND_PLAYER
+    } else {
+      this.currentPlayerId = FIRST_PLAYER
     }
   }
 
   _isAllMarked() {
-    const markedCount = 0;
+    let markedCount = 0;
 
     for (let columnIndex = 0; columnIndex < 3; columnIndex += 1) {
       for (let rowIndex = 0; rowIndex < 3; rowIndex += 1) {
@@ -254,9 +457,35 @@ class TicTacToe {
   _setupListerners() {
     TicTacToeNodes.getStartGameButton().onclick = (event) => {
       event.preventDefault();
+      
+      const playersNames = this.renderer.getPlayerNames();
+      this.setPlayers(playersNames[0], playersNames[1]);
+      
       this.startGame();
     }
-    // TODO
+    
+    TicTacToeNodes.getNodesWithPosition().forEach(({ node, position }) => {
+      node.onclick = (event) => {
+        event.preventDefault();
+        this.mark(position);
+      }
+    })
+
+    TicTacToeNodes.getResetMatchButton().onclick = () => {
+      this.startGame();
+    }
+
+    TicTacToeNodes.getResetScoreboardButton().onclick = () => {
+      this.resetScoreboard();
+    }
+
+    TicTacToeNodes.getChangePlayerNamesButton().onclick = () => {
+      this.onClickChangePlayerNamesButton();
+    }
+
+    TicTacToeNodes.getUpdatePlayersButton().onclick = () => {
+      this.onClickUpdatePlayersButton();
+    }
   }
 
   _setupWinPossibilities() {
@@ -276,15 +505,15 @@ class TicTacToe {
   }
 
   _setupPlayerMarkers() {
-    this.players.first.marker = X_MARK;
-    this.players.second.marker = O_MARK;
+    this.players[FIRST_PLAYER].marker = X_MARK;
+    this.players[SECOND_PLAYER].marker = O_MARK;
   }
 }
 
 class Player {
   constructor(name, score, marker) {
-    this.name = name
-    this.score = score
+    this.name = name || ""
+    this.score = score || 0
     this.marker = marker || null;
   }
 }
